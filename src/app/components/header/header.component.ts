f7d5781580cb920b2049cad23c31d0abdda3ee32
subotic.l <lukasubotic34@gmail.com>
2026-01-16 18:11:23 +0100

    feat: add map as a separate page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,17 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, MapComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   authService = inject(AuthService);
-  showMap = signal(false);
-
-  toggleMap(): void {
-    this.showMap.set(!this.showMap());
-  }
 
   onLogout(): void {
     this.authService.logout();
